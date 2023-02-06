ef20f4a7e285fc4e7e31790a7ad50da3b772ffe6
jmonlyade <jeremy.monlyade@gmail.com>
2023-02-06 20:31:30 +0100

    init commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FloorPlan from './components/floorplan/FloorPlan';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FloorPlan />
   </React.StrictMode>
 );
 
